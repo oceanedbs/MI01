4101e4887a9a00d7ca0bb37695fa47a363dc5c82
PERRIN Geoffrey <perrin.geoffrey.1@gmail.com>
2017-11-26 18:55:17 +0100

    ajout du code dans le rapport

--- TP04/conversion.asm
+++ TP04/conversion.asm
@@ -14,66 +14,57 @@ extern      getchar:near
 
 .data
 
-nombre      dd      95c8ah          ; Nombre à convertir
-chaine      db      35 dup(?)       ; Remplacer xx par la longueur maximale n de la chaîne
-chiffres db "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nombre      dd      95c8ah          ; Nombre ï¿½ convertir
+chaine      db      35 dup(?)       ; Remplacer xx par la longueur maximale n de la chaï¿½ne
+chiffres 		db 			"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 .code
 
-; Sous-programme main, automatiquement appelé par le code de
-; démarrage 'C'
+; Sous-programme main, automatiquement appelï¿½ par le code de
+; dï¿½marrage 'C'
 public      main
 main        proc
 			push 	eax				; sauvegarde des registres
-			push    ebx             ; Sauvegarde pour le code 'C'
+			push  ebx             ; Sauvegarde pour le code 'C'
 
-			xor	ebx, ebx
-			xor	eax, eax
-			mov	ecx, 35
-			mov	eax, [nombre]
-			cmp eax, 0
-			jge suivant
-			dec eax
-			not eax
+			xor		ebx, ebx
+			xor		eax, eax
+			mov		ecx, 35
+			mov		eax, [nombre]
+			cmp 	eax, 0
+			jge 	suivant
+			dec 	eax
+			not 	eax
 			push 	eax				; sauvegarde des registres
 			push	ecx
-			push    "-"       ; on affiche le - si le nombre est négatif
-            call    putchar     ; Appel de putchar
-            add     esp, 4      ; Nettoyage de la pile après appel
+			push  "-"       ; on affiche le - si le nombre est nï¿½gatif
+      call    putchar     ; Appel de putchar
+      add     esp, 4      ; Nettoyage de la pile aprï¿½s appel
 			pop ecx
 			pop eax
 
-suivant :		xor 	edx, edx		
+suivant :		xor 	edx, edx
 			div	ecx
-			
 			mov		[chaine+ebx], dl
-            inc		ebx
+      inc		ebx
 			cmp		eax, 0
 			jne		suivant
 			dec		ebx
-		
+
 
 affichage : movzx   eax, byte ptr[ebx + chaine]
 
-            ; Appel à la fonction de bibliothèque ‘C’ putchar(int c) 
-            ; pour afficher un caractère. La taille du type C 'int' 
-            ; est de 32 bits sur IA-32. Le caractère doit être fourni
-            ; sur la pile.
-			lea		edx, [chiffres + eax]
-            push    [edx]         ; Caractère à afficher
-            call    putchar     ; Appel de putchar
-            add     esp, 4      ; Nettoyage de la pile après appel
-            ; Fin de l'appel à putchar
-
-            dec     ebx             ; Caractère suivant
-            cmp     ebx,10 ; Toute la longueur ?
-            jb     affichage         ; si non, passer au suivant
-
-            call    getchar         ; Attente de l'appui sur "Entrée"
-
+			lea			edx, [chiffres + eax]
+      push   	[edx]         	; Caractï¿½re ï¿½ afficher
+      call    putchar     		; Appel de putchar
+      add     esp, 4      		; Nettoyage de la pile aprï¿½s appel
+  		dec     ebx             ; Caractï¿½re suivant
+	    cmp     ebx,10 					; Toute la longueur ?
+      jb     	affichage        ; si non, passer au suivant
+			call    getchar         ; Attente de l'appui sur "Entrï¿½e"
 			pop     ebx
-            ret                     ; Retour au code de démarrage 'C'
+      ret                     ; Retour au code de dï¿½marrage 'C'
 
 main       endp
 
-            end
+end
