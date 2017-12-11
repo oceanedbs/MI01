ee27bc30b87d33f16e79e2e35b60f40762070bc3
Unknown <oceane.tribudubois@gmail.com>
2017-12-11 19:09:59 +0100

    Code

--- TP06-07/image.asm
+++ TP06-07/image.asm
@@ -26,89 +26,174 @@
 PUBLIC      process_image_asm
 process_image_asm   PROC NEAR       ; Point d'entrée du sous programme
 
-  push    ebp
-  mov     ebp, esp
+   push    ebp
+        mov     ebp, esp
 
-  push    ebx
-  push	  eax
-  push   	edx
-  push    esi
-  push    edi
-
-  mov     ecx, [ebp + 8]
-  imul    ecx, [ebp + 12]
-
-  mov     esi, [ebp + 16]
-  mov     edi, [ebp + 20]
-
-;*****************************************************************
-;*****************************************************************
-; Ajoutez votre code ici
-;*****************************************************************
-;*****************************************************************
-        xor		 edx,edx
-boucle: mov		 ebx,[esi+ecx*4]
-        movzx	 eax,bl
-        mov		 dx,1Dh
-        mul		 dx
-        push	 eax
-
-        shr    ebx,8
-        movzx	 eax,bl
-        mov		 dx,96h
-        mul		 dx
-        push	 eax
-
-        shr		 ebx,8
-        movzx	 eax,bl
-        mov		 dx,4Ch
-        mul		 dx
-        pop		 ebx
-        add		 eax,ebx
-        pop		 ebx
-        add		 eax,ebx
-        shr		 eax,8
-        mov		 [edi+ecx*4],eax
+        push		ecx
+        push        eax
+        push        edx
 
 
-        dec    ecx
+        ;récupération des arguments dans les différents registres
+        mov     ecx, [ebp + 8]
+        imul    ecx, [ebp + 12]
+
+        mov     esi, [ebp + 16]
+        mov     edi, [ebp + 20]
+
+        xor                 edx,edx
+        ;début de la boucle
+boucle: dec      ecx
+        ;on récupére le pixel de l'image source à traiter
+        mov                 ebx,[esi+ecx*4]
+
+        ;-------------------
+        ;calcul de B * 0,114
+        ;-------------------
+
+        mov             eax,ebx
+        ;on récupere la composante bleu dans eax
+        and      eax,000000FFh
+        ;on effectue le calcul
+        imul         eax,1Dh ;B * 0,114
+        ;on stock la somme (I) dans edx
+        mov      edx,eax
+
+        ;-------------------
+        ;calcul de V *,0,587
+        ;-------------------
+
+        ;on récupere la composante verte dans eax
+        mov             eax,ebx
+        and      eax,0000FF00h
+        shr                 eax,8
+        ;on effectue le calcul
+        imul         eax,96h ;V *,0,587
+        add      edx,eax ;I=I+V *,0,587
+
+        ;-------------------
+        ;calcul de R*0,299
+        ;-------------------
+        ;on récupere la composante rouge dans eax
+        mov             eax,ebx
+        and      eax,00FF0000h
+        shr                 eax,16
+        ;on effectue le calcul
+        imul         eax,4Ch
+        add      edx,eax ;I=I+R*0,299
+
+        ;on divise par 256
+        shr                 edx,8
+        ;on stock I dans la composante bleu de l'image destination
+        mov      [edi+ecx*4],edx
+
+
         cmp    ecx, 0
         ja     boucle
 
-		mov    esi, [ebp + 16] ; 1er pixel img source
-		mov		ebx, [ebp + 12]
-		sub		ebx, 2   ; nb de lignes
 
-		push	ebp ; sauvegarde de ebp
-		mov    ebp, [ebp + 8] ; on récupère la largeur
+		;tp6
+		mov ecx,[ebp+12]
+		sub ecx,2
+		shl ecx,16
+
+		mov esi,edi
+		mov     edi, [ebp + 24]
+		mov     ebp, [ebp + 8] ; on récupère la largeur
 		mov		eax,ebp
 		shl		eax,2		; on multiplie la largeur par 4
-		sub		ebp, 2	; on enlve la première et la dernière colone
 		add		edi,eax ; on ajoute au premier pixel de l'image tmp2 4*largeur
-		
-		mov		ecx, ebx
-		shl		ecx, 8 ; initialisation du compteur de lignes
+		add		edi, 4
 		mov		cx,bp
-		boucle3:
-		sub	ecx, 256
-		test	ecx,11111111111111110000000000000000b
-		boucle2:
-		sub		ecx,1
-		mov [edi+cx],11111111111111110000000000000000b
-		mov		esi,[esi+ebp+2]
-		mov		edi,[edi+ebp]
-		test	ecx,00000000000000001111111111111111b
-		jnz boucle2
+		sub ecx, 00010000h
+
+	
+	boucle3:
+		add		ecx,ebp
+		sub		ecx,2
+	
+	boucle2:
+		;push ebx
+		;mov	ebx, 00FF0000h
+
+		;mov [edi],ebx
+		;pop	ebx
+
+		xor		ebx, ebx
+		mov		ebx, esi
+		imul	ebx,-1
+		add		ebx,[esi+8]
+		mov		eax,[esi+ebp*4]
+		imul	eax,-2
+		add		ebx,eax
+		mov		eax,[esi+ebp*4+8]
+		imul	eax,2
+		add		ebx,eax
+		mov		eax,[esi+ebp*8]
+		imul	eax,-1
+		add		ebx,eax
+		add		ebx,[esi+ebp*8+8]
+
+		xor		edx, edx
+		mov		edx, esi
+		mov		eax,[esi+4]
+		imul	eax,2
+		add		edx, eax
+		mov		eax,[esi+8]
+		add		edx, eax
+		mov		eax,[esi+ebp*8]
+		imul	eax,-1
+		add		edx,eax
+		mov		eax,[esi+ebp*4+4]
+		imul	eax,-2
+		add		edx,eax
+		mov		eax,[esi+ebp*8+8]
+		imul	eax,-1
+		add		ebx,eax
+		add		edx,[esi+ebp*8+8]
+
+		test	ebx,0
+		jg		gx_positif
+		imul ebx,-1
+	
+	gx_positif :
+		test	edx, 0
+		jg		gy_positif
+		imul	edx, -1
+
+	gy_positif :
+		add ebx, edx
+		xor eax, eax
+		mov	eax, 255
+		sub eax, ebx
+		test	eax, 0
+		jg		g_positif
+		xor eax,eax
+		g_positif:
+
+		mov	[edi], eax
+		
+
+		add esi, 4
+		add edi, 4
+		dec ecx
+		cmp cx, 0
+		jne	   boucle2
+
+
+		add esi, 8
+		add edi, 8
+		sub ecx, 00010000h
 		jnz    boucle3
 
 fin:
-pop     edi
-pop     esi
-pop		  edx
-pop		  eax
-pop     ebx
-pop     ebp
-ret                         ; Retour ï¿½ la fonction MainWndProc
+		
+		pop     edx
+        pop     eax
+        pop     ecx
+		pop     ebp
+       
+        ret                         ; Retour à la fonction MainWndProc
 
 process_image_asm   ENDP
 END
